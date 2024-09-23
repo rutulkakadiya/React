22cbf8791cb42717a3d479803c3f563b5f2f29c2
rutulkakadiya <rutulkakadiya973@gmail.com>
2024-09-23 17:39:53 +0530

    Day 20 Local Storage

--- Day 20 Local Storage/src/Components/LocalStorage.jsx
+++ Day 20 Local Storage/src/Components/LocalStorage.jsx
@@ -7,16 +7,24 @@ export default function LocalStorage() {
 
     localStorage.removeItem("Name")
 
-    let obj = {"Name" : "Rutul Kakadiya" , "Subject" : "IT"}
+    let obj = {Name : "Rutul Kakadiya" , Subject : "IT"}
 
     localStorage.setItem("MyObj" , JSON.stringify(obj));
     
     let objData = JSON.parse(localStorage.getItem("MyObj"))
 
+    let obj1 = {Name : "Ayush Ramoliya" , Subject : "Account"}
+
+    localStorage.setItem("MyObj1" , JSON.stringify(obj1))
+
+    let objData1 = JSON.parse(localStorage.getItem("MyObj1"))
+
+
     console.log(objData)            
     return (
         <div>
-            <p>vc</p>
+            <p>{objData.Name}</p>
+            <p>{objData1.Subject}</p>
         </div>
     )
 }
