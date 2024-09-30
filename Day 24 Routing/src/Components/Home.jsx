e202efef43c22e7866ce4366fb33196465772d9f
rutulkakadiya <rutulkakadiya973@gmail.com>
2024-09-30 17:50:47 +0530

    Day Routing

--- Day 24 Routing/src/Components/Home.jsx
+++ Day 24 Routing/src/Components/Home.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <div>
+      
       <h1>Home</h1>
       <Link to={'/About'}>About</Link>
       <Link to={'/Contact'}>Contact</Link>
