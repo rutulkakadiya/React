037e14b6e4a43a98d77d8b4ec9204148c74ad625
rutulkakadiya <rutulkakadiya973@gmail.com>
2024-10-07 16:49:19 +0530

    Day 27

---
+++ Day 27 Filtering , Searching and Sorting/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Searching from './Components/Searching'
+
+export default function App() {
+  return (
+    <div>
+      <Searching/>
+    </div>
+  )
+}
---
+++ Day 27 Filtering , Searching and Sorting/src/Components/Searching.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+export default function Searching() {
+    
+    const [name , setName] = useState("");
+
+    const arr = [
+        {name : "Rutul" , subject : "React"},
+        {name : "Ayush" , subject : "React"},
+        {name : "Vrushil" , subject : "React"},
+        {name : "Khushal" , subject : "React"},
+        {name : "Utsav" , subject : "React"},
+        {name : "Kartik" , subject : "React"},
+        {name : "Saurabh" , subject : "React"},
+    ]
+
+    const data = arr.filter((item)=> item.name.includes(name))
+  return (
+    <div>
+      <input type="text" placeholder='Enter name' onChange={(e)=> setName(e.target.value)} />
+
+      {
+        data &&
+        data.map((e,i)=>{
+            return <ul key={i}>
+                <li>{e.name}</li>
+                <li>{e.subject}</li>
+            </ul>
+        })
+      }    
+    </div>
+  )
+  
+  
+}
