f6929dac61313ba2da0689f90c4a810b4e3dbd0e
rutulkakadiya <rutulkakadiya973@gmail.com>
2024-10-14 23:11:44 +0530

    Day 20

--- Day 30 Redux CRUD/src/Redux/ReduxCounter.jsx
+++ Day 30 Redux CRUD/src/Redux/ReduxCounter.jsx
@@ -1,22 +1,86 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addData, deleteData, updateData } from '../features/TodoSlice';
 
 export default function ReduxCounter() {
+  const [name, setName] = useState('');
+  const [subject, setSubject] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
 
-    const [name, setName] = useState("");
-    const [subject, setSubject] = useState("");
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const count = useSelector((state) => state.todoKey);
 
-    const addRecord = () => {
-        dispatch(addRecord({id: Date.now(), name ,subject }))
+  const addRecord = () => {
+    if (editIndex == null) {
+      dispatch(addData({ id: Date.now(), name, subject }));
+      setName('');
+      setSubject('');
+    } else {
+      dispatch(updateData({ id: editIndex, name, subject }));
+      setEditIndex(null);
     }
-    return (
-        <div>
-            <input type="text" placeholder='Enter name' onClick={(e) => setName()} /><br /><br />
-            <input type="text" placeholder='Enter subject' onClick={(e) => setSubject()} />
-            <br /><br />
-            <button onClick={addRecord}>Add</button>
-        </div>
-    )
+  };
+
+  const deleteRecord = (id) => {
+    dispatch(deleteData(id));
+  };
+
+  const editRecord = (id) => {
+    let singleData = count.students.find((item) => item.id == id);
+    setName(singleData.name);
+    setSubject(singleData.subject);
+    setEditIndex(id);
+  };
+
+  return (
+    <div className="container">
+      <input
+        type="text"
+        placeholder="Enter name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="Enter subject"
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
+      />
+      <br />
+      <button onClick={addRecord}>{editIndex ? 'Update' : 'Add'}</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Subject</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {count.students.map((e, i) => (
+            <tr key={i}>
+              <td>{e.id}</td>
+              <td>{e.name}</td>
+              <td>{e.subject}</td>
+              <td>
+                <button className="edit-btn" onClick={() => editRecord(e.id)}>
+                  Edit
+                </button>
+              </td>
+              <td>
+                <button className="delete-btn" onClick={() => deleteRecord(e.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- Day 30 Redux CRUD/src/features/TodoSlice.jsx
+++ Day 30 Redux CRUD/src/features/TodoSlice.jsx
@@ -1,15 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-export const todo = createSlice ({
-    name : "todo",
-    initialState : {students : []},
-    reducers:{
-        addData :{
-            
+export const todo = createSlice({
+    name: "todo",
+    initialState: { students: [] },
+    reducers: {
+        addData: (state, action) => {
+            state.students.push(action.payload)
+        },
+        deleteData: (state, action) => {
+            const data = state.students.filter((item) => item.id != action.payload)
+            state.students = data
+        },
+        updateData: (state, action) => {
+            state.students.map((e,i)=>{
+                if(e.id == action.payload.id){
+                    e.name = action.payload.name
+                    e.subject = action.payload.subject
+                }
+                else{
+                    e
+                }
+            })
         }
     }
 })
 
-export const {addData} = todo.actions
+export const { addData, deleteData , updateData } = todo.actions
 export default todo.reducer
